9e7173e62c7aa6d717f87afc7845718cb76c8fa5
Mateusssi <mateuspetris@gmail.com>
2026-09-14 22:16:28 -0300

    chore: cria ambiente do spring para inicialização do projeto

---
+++ src/main/java/meuprojeto/pesquisaOrdenacao/PesquisaOrdenacaoApplication.java
@@ -0,0 +1,13 @@
+package meuprojeto.pesquisaOrdenacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesquisaOrdenacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesquisaOrdenacaoApplication.class, args);
+	}
+
+}
